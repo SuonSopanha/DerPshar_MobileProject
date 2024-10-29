0d3a7b610741effe6849c422001b14057a5641a6
SkyK.O <149860914+KOLADDR9@users.noreply.github.com>
2024-10-29 22:14:11 +0700

    HomeScreen

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './navigation/TabNavigator';
+import { View, Text } from 'react-native';
+import { tailwind } from 'tailwindcss-react-native';
 
 export default function App() {
   return (
