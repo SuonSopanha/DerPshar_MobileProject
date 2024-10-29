f4c80ef32f57403dba8421e35f93fbdbdf83dd9d
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-10-29 22:55:12 +0700

    remove unessesary packages

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './navigation/TabNavigator';
 import { View, Text } from 'react-native';
-import { tailwind } from 'tailwindcss-react-native';
 
 export default function App() {
   return (
